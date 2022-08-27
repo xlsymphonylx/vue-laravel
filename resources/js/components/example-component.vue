b76262be837b8769e8da392abb97ae74913af044
xlsymphonylx <xlsymphonylx@gmail.com>
2022-08-27 12:55:19 -0600

    initial 2

---
+++ resources/js/components/example-component.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">CRUD CUSTOMER</div>
+
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr
+                                    v-for="customer in customers"
+                                    :key="customer"
+                                ></tr>
+                            </thead>
+                            <tbody>
+                                <tr></tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "example-component",
+    data: () => ({
+        customers: [],
+        customerForm: {},
+    }),
+    mounted() {
+        console.log("Component mounted.");
+    },
+};
+</script>
